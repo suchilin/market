e72ae80ce87a43cae4c71c5f7e43aa69a09eaba6
suchil <>
2016-10-13 23:25:58 -0500

    8)

--- products/models.py
+++ products/models.py
@@ -7,16 +7,26 @@ from django.db import models
 
 class Unit(models.Model):
     name = models.CharField(max_length=30, unique=True)
-    symbol = models.CharField(max_lenght=5)
+    symbol = models.CharField(max_length=5)
     description = models.TextField()
+
+    def __unicode__(self):
+        return self.name
 
 class Feature(models.Model):
     name = models.CharField(max_length=30)
     quantity = models.FloatField()
-    units = models.OneToOneField(Units, related_name='unit_of')
+    units = models.OneToOneField(Unit, related_name='unit_of')
     description = models.TextField()
+
+    def __unicode__(self):
+        return self.name
 
 class Product(models.Model):
     name = models.CharField(max_length=30)
     description = models.TextField()
     features = models.ManyToManyField(Feature, related_name='features')
+    components = models.ManyToManyField('self', null=True, blank=True)
+
+    def __unicode__(self):
+        return self.name
